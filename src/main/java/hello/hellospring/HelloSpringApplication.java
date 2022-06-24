6b488eeca8731823fc65e4be91b0e6b8eb30cfd3
namhr <namhr1234@hanatour.com>
2022-06-24 19:22:26 +0900

    빌드를 인텔리제이로 하는 방법 주석추가

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
+	/*
+	 Preference에서 gradle 선택 --> Build and run using and Test and run using을 IntellJ로 변경함/
+	 */
 }
