a3fdfad3d8e11a05967e4cd89ee430f9923318cf
namhr <namhr1234@hanatour.com>
2022-06-27 23:15:46 +0900

    mvc 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,10 @@
 package hello.hellospring.controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,6 +13,12 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","hello!!!");
         return "hello"; // model의 data를 넘기면서 resource/template/hello.html을 실행시켜라
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template"; //http://localhost:8080/hello-mvc?name=spring
     }
 
 }
